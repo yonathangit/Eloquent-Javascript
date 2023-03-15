a2bc207d55827783191d2e3227dad930ab0c4785
Yonathan Berhanu <yonathan.berhanu17@gmail.com>
2023-03-15 15:30:20 +0300

    deep_comparison

--- Chapter Four/Lessons/Exercises/deep_comparison.js
+++ Chapter Four/Lessons/Exercises/deep_comparison.js
@@ -35,7 +35,13 @@ let b = {
     name: "Yoni"
 };
 
-console.log(deepEqual(a,b));
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
 
 // let obj = {
 //     value: 1,
